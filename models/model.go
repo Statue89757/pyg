3551b091e527796488546e607b9d817d518cbcc6
Statue89757 <18655554172@163.com>
2019-07-17 17:53:06 +0800

    1.0

--- models/model.go
+++ models/model.go
@@ -7,23 +7,23 @@ import (
 
 type User struct{ //用户表
 	Id 			int
-	Name 		string`orm:"size(20);unique"` 		//用户名
-	PassWord 	string`orm:"size(20)"`		//登陆密码
-	Email 		string`orm:"size(50)"`   	//邮箱
-	Active 		bool`orm:"default(false)"`    	//是否激活
-	Power 		int`orm:"default(0)"`			//权限设置  0 表示未激活  1表示激活
-	Address		[]*Address `orm:"reverse(many)"`
+	Name 		string       `orm:"size(20);unique"` 		//用户名
+	PassWord 	string         `orm:"size(20)"`		//登陆密码
+	Email 		string           `orm:"size(50)"`   	//邮箱
+	Active 		bool            `orm:"default(false)"`    	//是否激活
+	Power 		int             `orm:"default(0)"`			//权限设置  0 表示未激活  1表示激活
+	Address		[]*Address       `orm:"reverse(many)"`
 
 }
 
 type Address struct { //地址表
 	Id int
-	Receiver string `orm:"size(20)"`  			//收件人
-	Addr string		`orm:"size(50)"`  			//收件地址
-	Zipcode string  `orm:"size(20)"` 			//邮编
-	Phone string	 `orm:"size(20)"` 			//联系方式
-	Isdefault bool	 `orm:"defalt(false)"`			//是否默认 0 为非默认  1为默认
-	User *User `orm:"rel(fk)"` 	//用户ID
+	Receiver string          `orm:"size(20)"`  			//收件人
+	Addr string		           `orm:"size(50)"`  			//收件地址
+	Zipcode string          `orm:"size(20)"` 			//邮编
+	Phone string	         `orm:"size(20)"` 			//联系方式
+	Isdefault bool	          `orm:"defalt(false)"`			//是否默认 0 为非默认  1为默认
+	User *User                   `orm:"rel(fk)"`                      	//用户ID
 
 }
 
@@ -31,6 +31,8 @@ type Address struct { //地址表
 func init() {
 	//先在mysql中新建一个数据库(务必设置charset=utf8),然后登录mysql中对应的数据库,其中登录时,需要对参数 mysql进行手动导包:_空格
 	orm.RegisterDataBase("default","mysql","root:123456@tcp(127.0.0.1:3306)/pyg")
+  //注册对应的表
    orm.RegisterModel(new(User),new(Address))
+   //跑起来
    orm.RunSyncdb("default",false,true)
 }
